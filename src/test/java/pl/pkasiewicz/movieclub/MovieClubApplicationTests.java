14c3f666f038a3a7667c98e762c613bfc84673d5
piotrkasiewicz <ptrkasiewicz@gmail.com>
2025-02-13 01:14:06 +0100

    Initial commit

---
+++ src/test/java/pl/pkasiewicz/movieclub/MovieClubApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pkasiewicz.movieclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieClubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
